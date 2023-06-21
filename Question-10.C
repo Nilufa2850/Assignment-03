0d14954e9c2936275b462955de5eab78bd1f64fe
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 12:56:55 +0530

    Create Question-10.C

---
+++ Question-10.C
@@ -0,0 +1,23 @@
+/*Write a program which takes the cost price and selling price of a product from the
+user. Now calculate and print profit or loss percentage.*/
+
+#include<stdio.h>
+int main()
+{
+    float cp , sp , percentage;
+    printf("Enter enter cost & selling prices : ");
+    scanf("%f%f",&cp,&sp);
+
+    if (sp>cp)
+    {
+        printf("\nProfit");
+        printf("\nProfit Percentage is %f", (sp-cp)/cp*100 );
+    }
+    else 
+    {
+        printf("\nLoss");
+        printf("\nLoss Percentage is %f", (cp-sp)/cp*100 );
+    }   
+
+    return 0;
+}
